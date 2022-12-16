8c7fa83016bc9fbb330ad5a089dcd592cd07a522
troygilmore04 <103476955+troygilmore04@users.noreply.github.com>
2022-12-16 01:11:24 -0500

    initial commit

---
+++ src/server/trpc/router/_app.ts
@@ -0,0 +1,10 @@
+import { router } from "../trpc";
+import { authRouter } from "./auth";
+
+
+export const appRouter = router({
+  auth: authRouter,
+});
+
+// export type definition of API
+export type AppRouter = typeof appRouter;
